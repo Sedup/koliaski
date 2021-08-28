0e6b632316a1ed0e4a0c403403d86299e45a03e1
Sedup <69250193+Sedup@users.noreply.github.com>
2021-08-28 20:26:36 +0300

    end of slider section

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
 var swiper = new Swiper('.swiper-container', {
   direction: 'horizontal',
-  loop: false,
-  autoHeight: false,
+  loop: true,
+  autoHeight: true,
   slidesPerView: '4',
-  slidesPerGroup: '4',
-  slidesPerColumn: '2',
+  //slidesPerGroup: '1',
+  //spaceBetween: '20',
+  slideToClickedSlide: true,
+  autoHight: true,
+  //slidesPerColumn: '2',
 
   watchOverflow: true,
 
@@ -21,18 +24,49 @@ var swiper = new Swiper('.swiper-container', {
 
 });
 
+//swiper.destroy();
+
 // tabs
 $(document).ready(function () {
   var sliderButtons = $(".sliders-buttons_item");
   var slider = $(".slider");
-  console.log("click");
+
 
   sliderButtons.on("click", function (event) {
-    swiper.destroy(true, true);
     var activeSlider = $(this).attr("data-target");
     sliderButtons.removeClass("activeButton");
     slider.removeClass("slider-active");
     $(activeSlider).addClass("slider-active");
     $(this).addClass("activeButton");
+
+
+    swiper = new Swiper('.swiper-container', {
+      direction: 'horizontal',
+      loop: true,
+      autoHeight: true,
+      slidesPerView: '4',
+      //slidesPerGroup: '1',
+      //spaceBetween: '20',
+      slideToClickedSlide: true,
+      autoHight: true,
+      //slidesPerColumn: '2',
+
+      watchOverflow: true,
+
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true,
+      },
+
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+
+    });
+
+
+
   });
 });
